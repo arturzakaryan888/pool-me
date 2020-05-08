f56db0d7af73b3bd23cf3b6d0e13f640f5ca246e
Artur <arturzakaryan888@gmail.com>
2020-05-08 23:42:23 +0400

    create user controller

---
+++ src/main/java/com/rau/poolme/demo/controller/UserRestController.java
@@ -0,0 +1,28 @@
+package com.rau.poolme.demo.controller;
+
+
+import com.rau.poolme.demo.model.Users;
+import com.rau.poolme.demo.service.users.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/user/")
+public class UserRestController {
+    @Autowired
+    private UsersService usersService;
+
+    @RequestMapping(value = "save",method = RequestMethod.POST,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity save(@RequestBody @Valid Users users){
+        usersService.save(users);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+
+}
